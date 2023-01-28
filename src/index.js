6ad33a209275c5a8e8efbbb58b96e3062f79dd22
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-01-28 14:06:44 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import * as React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Calendar from 'react-calendar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
